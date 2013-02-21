a524c9d838284e2844919ee831bb39a50acd7793
Russell Smith <qqrsmith@gmail.com>
2013-02-21 16:42:28 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Usedbookscraper::Application.config.secret_token = 'cb44c37695c78e8ef603f666d04c8376d9c7204e79aeb3ae95d09628710a8dee3f559a2131a1d109b9aefb3c261b362ff1ca3b429acb695da04419d13993a9d4'
