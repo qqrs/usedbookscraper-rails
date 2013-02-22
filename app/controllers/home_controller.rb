88d6fb591fa76fa2b778bd5f8af6e790858c14f6
Russell Smith <qqrsmith@gmail.com>
2013-02-21 21:44:04 -0500

    read list of shelves for user from goodreads

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
+require 'goodreads'
+
 class HomeController < ApplicationController
   def index
   end
@@ -8,5 +10,8 @@ class HomeController < ApplicationController
 
     # TODO: need to escape this?
     @goodreads_user_id = params[:goodreads_user_id]
+
+    gr = Goodreads.new(api_key: ENV["GOODREADS_KEY"]) 
+    @user_shelves = gr.user(@goodreads_user_id).user_shelves
   end
 end
