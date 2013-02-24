8052215ad064165fb136cb05d1a7f04c91660e08
Russell Smith <qqrsmith@gmail.com>
2013-02-23 23:06:55 -0500

    show books for first selected shelf

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,14 +12,35 @@ class HomeController < ApplicationController
     @goodreads_user_id = params[:goodreads_user_id]
 
     gr = Goodreads.new(api_key: ENV["GOODREADS_KEY"]) 
-    @user_shelves = gr.user(@goodreads_user_id).user_shelves
+    @shelves = gr.user(@goodreads_user_id).user_shelves
 
     # mock shelf names for testing
-    #@user_shelves = (1..10).map{|n| Hashie::Mash.new({name: "shelf #{n}"}) }
+    #@shelves = (1..10).map{|n| Hashie::Mash.new({name: "shelf #{n}"}) }
 
   end
 
   def books
-    @shelves = params[:shelves]
+    goodreads_user_id = params[:goodreads_user_id]
+    shelves = params[:shelves]
+
+    gr = Goodreads.new(api_key: ENV["GOODREADS_KEY"]) 
+
+    shelf = gr.shelf(goodreads_user_id, shelves[0])
+    @book_titles = shelf.books.map{|b| b.book.title}
+    @books = shelf.books.map do |b|
+      Hashie::Mash.new(
+        { 
+          title:        b.book.title,
+          author:       b.book.authors.author.name,
+          isbn:         b.book.isbn,
+          image_url:    b.book.image_url,
+          owned:        b.owned,
+          link:         b.link
+        }
+      )
+    end
+
+
+    @books_debug = shelf if Rails.env.development?  
   end
 end
