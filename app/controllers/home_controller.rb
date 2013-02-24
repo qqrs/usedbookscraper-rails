dca9d513a305e9a00fb9040fe4eb092b223e7a7c
Russell Smith <qqrsmith@gmail.com>
2013-02-23 23:42:26 -0500

    show books for all shelves

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -24,23 +24,28 @@ class HomeController < ApplicationController
     shelves = params[:shelves]
 
     gr = Goodreads.new(api_key: ENV["GOODREADS_KEY"]) 
+    @books = []
 
-    shelf = gr.shelf(goodreads_user_id, shelves[0])
-    @book_titles = shelf.books.map{|b| b.book.title}
-    @books = shelf.books.map do |b|
-      Hashie::Mash.new(
-        { 
-          title:        b.book.title,
-          author:       b.book.authors.author.name,
-          isbn:         b.book.isbn,
-          image_url:    b.book.image_url,
-          owned:        b.owned,
-          link:         b.link
-        }
-      )
+    shelves.each do |shelf_name|
+      # TODO: goodreads api paginates -- this only gets first 20 books per shelf
+      shelf = gr.shelf(goodreads_user_id, shelf_name)
+      shelf.books.each do |b|
+        @books << Hashie::Mash.new(
+          { 
+            title:        b.book.title,
+            author:       b.book.authors.author.name,
+            isbn:         b.book.isbn,
+            image_url:    b.book.image_url,
+            owned:        b.owned,
+            link:         b.link
+          }
+        )
+      end
+
+      @books_debug = shelf if Rails.env.development?  
     end
 
+    @books.uniq!{|b| b.isbn}
 
-    @books_debug = shelf if Rails.env.development?  
   end
 end
